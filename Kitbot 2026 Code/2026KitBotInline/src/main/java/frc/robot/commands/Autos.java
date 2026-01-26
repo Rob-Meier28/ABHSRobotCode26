25eea38f657fd0a23d60fa1c04daedfbba26cb25
Noah-M12 <meiern0827@gmail.com>
2026-01-26 13:35:12 -0500

    Code Buildable

--- Kitbot 2026 Code/2026KitBotInline/src/main/java/frc/robot/commands/Autos.java
+++ Kitbot 2026 Code/2026KitBotInline/src/main/java/frc/robot/commands/Autos.java
@@ -7,16 +7,16 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.CANFuelSubsystem;
-import frc.robot.subsystems.CANDriveSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 
 public final class Autos {
   // Example autonomous command which drives forward for 1 second.
-  public static final Command exampleAuto(CANDriveSubsystem driveSubsystem, CANFuelSubsystem ballSubsystem) {
-    return new SequentialCommandGroup(
-        // Drive backwards for .25 seconds. The driveArcadeAuto command factory
+  public static final Command exampleAuto(DriveSubsystem driveSubsystem, CANFuelSubsystem ballSubsystem) {
+    return new SequentialCommandGroup();
+       /*  // Drive backwards for .25 seconds. The driveArcadeAuto command factory
         // creates a command which does not end which allows us to control
         // the timing using the withTimeout decorator
-        driveSubsystem.driveArcade(() -> 0.5, () -> 0).withTimeout(.25),
+        driveSubsystem.drive(() -> 0.5, () -> 0).withTimeout(.25),
         // Stop driving. This line uses the regular driveArcade command factory so it
         // ends immediately after commanding the motors to stop
         driveSubsystem.driveArcade(() -> 0, () -> 0),
@@ -26,5 +26,6 @@ public final class Autos {
         ballSubsystem.launchCommand().withTimeout(9),
         // Stop running the launcher
         ballSubsystem.runOnce(() -> ballSubsystem.stop()));
+        */
   }
 }
