0563b78a3c062c15ade64694470c579cc6c9a99c
Rob-Meier28 <RMeier@abs.misd.net>
2026-02-09 14:56:38 -0500

    Lower max speed
    Speed may be raised once bumpers are installed

--- Kitbot 2026 Code/SwerveDrive/src/main/java/frc/robot/Constants.java
+++ Kitbot 2026 Code/SwerveDrive/src/main/java/frc/robot/Constants.java
@@ -25,8 +25,8 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 5.74 ; //CHANGE THIS 
-    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+    public static final double kMaxSpeedMetersPerSecond = 3 ; //Lowered
+    public static final double kMaxAngularSpeed = 1 * Math.PI; // radians per second
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(27.5); // CHANGE THIS 
