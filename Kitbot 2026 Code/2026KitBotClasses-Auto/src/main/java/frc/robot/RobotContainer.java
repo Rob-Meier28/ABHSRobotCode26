fe7d0ff5f038710085f46c55b23cc680c4d05368
Rob-Meier28 <RMeier@abs.misd.net>
2026-02-18 11:08:11 -0500

    Added auto chooser to dashboard

--- Kitbot 2026 Code/2026KitBotClasses-Auto/src/main/java/frc/robot/RobotContainer.java
+++ Kitbot 2026 Code/2026KitBotClasses-Auto/src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -50,6 +51,8 @@ public class RobotContainer {
     // add additional auto modes you can add additional lines here with
     // autoChooser.addOption
     autoChooser.setDefaultOption("Autonomous", new ExampleAuto(driveSubsystem, fuelSubsystem));
+
+    SmartDashboard.putData("autoChooser", autoChooser);
   }
 
   /**
