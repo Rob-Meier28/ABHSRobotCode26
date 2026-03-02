902c46798433768f177200ff74fc5ea8aa2568bd
Rob-Meier28 <RMeier@abs.misd.net>
2026-03-02 11:11:11 -0500

    Removed unused import

--- Kitbot 2026 Code/2026KitBotClasses-ClimberImplementation/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ Kitbot 2026 Code/2026KitBotClasses-ClimberImplementation/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -8,7 +8,6 @@ import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
