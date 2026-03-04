4d287e9f16971f92a5cf877752cbb47f413ae2f9
Rob-Meier28 <RMeier@abs.misd.net>
2026-03-04 10:32:02 -0500

    Created copy for kitbot auto development

---
+++ Kitbot 2026 Code/2026KitBotClasses-AutoDev/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.FuelConstants.*;
+
+public class CANFuelSubsystem extends SubsystemBase {
+  private final SparkMax feederRoller;
+  private final SparkMax intakeLauncherRoller;
+
+  /** Creates a new CANBallSubsystem. */
+  public CANFuelSubsystem() {
+    // create brushless motors for each of the motors on the launcher mechanism
+    intakeLauncherRoller = new SparkMax(intakeLaunchMotorCanId, MotorType.kBrushless);
+    feederRoller = new SparkMax(hopperMotorCanId, MotorType.kBrushless);
+
+    // create the configuration for the feeder roller, set a current limit and apply
+    // the config to the controller
+    SparkMaxConfig feederConfig = new SparkMaxConfig();
+    feederConfig.inverted(true);
+    feederConfig.smartCurrentLimit(hopperMotorCurrentLimit);
+    feederRoller.configure(feederConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // create the configuration for the launcher roller, set a current limit, set
+    // the motor to inverted so that positive values are used for both intaking and
+    // launching, and apply the config to the controller
+    SparkMaxConfig launcherConfig = new SparkMaxConfig();
+    launcherConfig.inverted(true);
+    launcherConfig.smartCurrentLimit(launcherMotorCurrentLimit);
+    intakeLauncherRoller.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // put default values for various fuel operations onto the dashboard
+    // all commands using this subsystem pull values from the dashbaord to allow
+    // you to tune the values easily, and then replace the values in Constants.java
+    // with your new values. For more information, see the Software Guide.
+    SmartDashboard.putNumber("Intaking feeder roller value", intakingHopperVoltage);
+    SmartDashboard.putNumber("Intaking intake roller value", intakingIntakeVoltage);
+    SmartDashboard.putNumber("Launching feeder roller value", launchingHopperVoltage);
+    SmartDashboard.putNumber("Launching launcher roller value", launchingLauncherVoltage);
+    SmartDashboard.putNumber("Spin-up feeder roller value", spinUpHopperVoltage);
+  }
+
+  // A method to set the voltage of the intake roller
+  public void setIntakeLauncherRoller(double voltage) {
+    intakeLauncherRoller.setVoltage(voltage);
+  }
+
+  // A method to set the voltage of the intake roller
+  public void setFeederRoller(double voltage) {
+    feederRoller.setVoltage(voltage);
+  }
+
+  // A method to stop the rollers
+  public void stop() {
+    feederRoller.set(0);
+    intakeLauncherRoller.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
